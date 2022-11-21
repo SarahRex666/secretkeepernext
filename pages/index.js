aaeb28807ef61e6f7e7498c3f6218943177d3756
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-11-21 14:56:20 -0500

    home fixed

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,10 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function Home() {
-  const [ userMessage, setUserMessage ] = useState('')
 
-  console.log(userMessage)
-
-  const handleChange = (e) => {
-    setUserMessage(e.target.value)
-  }
-
-    const handleSubmit = (event) => {
-    event.preventDefault()
-    fetch("https://secretkeeperproject.herokuapp.com/message", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user_id: 3,
-        message: userMessage,
-      }),
-    })
-      .then((r) => r.json()).then((res) => console.log(res))
-  }
   
   return (
     <div className={styles.container}>
-      <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="tell me a secret" onChange={handleChange}/>
-      <input type="Submit" />
-      </form>
     </div>
   )
 }
